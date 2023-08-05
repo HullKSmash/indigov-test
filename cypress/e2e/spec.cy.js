1567cecfc3819e0b5afb64f7c754a51be494a4ee
Katie Hull <katie.ann.hull@gmail.com>
2023-08-05 13:24:58 -0700

    Add beforeEach hook, comments, and semicolons for readability

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,27 +12,35 @@ const expectedTsRepos = '5';
 var repoNames = [];
 var sortedRepoNames = [];
 
-describe('test github repos', () => {
+describe('Github Landing Page and Total Repos', () => {
   it('should visit Indigov landing page', () => {
     cy.get('main').find('header').find('h1').should('contain.text', orgName);
-  })
+  });
 
   it('contains the expected number of repos', () => {
     cy.get(repositorySelector).find(repositoryCountSelector).should('contain.text', expectedTotalRepos);
-  })
+  });
+});
 
-  it('contains the expected number of TypeScript repos', () => {
+describe('Sort and Filter Repos', () => {
+  beforeEach(() => {
+    cy.get(orgRepositoriesListSelector).find('ul').as('repoList');
     cy.get(repoFilterDropdownSelector).contains('Language').click();
     cy.get(repoFilterItemSelector).contains('TypeScript').click();
+  });
+
+  it('contains the expected number of TypeScript repos', () => {
     cy.get(orgRepositoriesListSelector).find('div').contains('results').should('contain.text', expectedTsRepos);
-  })
+  });
   
-  it('sorts the repos by name, descending', () => {
+  it('sorts the TS repos by name, descending', () => {
     cy.get(repoFilterDropdownSelector).contains('Sort').click();
     cy.get(repoFilterItemSelector).contains('Name').click();
-    cy.wait(1000); //wait for list to re-render after sort
-
-    cy.get(orgRepositoriesListSelector).find('ul').as('repoList');
+    
+    //Have to wait for list to re-render after sort.  Cypress does this implicitly on 
+    //a new page redirect, but not for this type of click.  The list is available 
+    //in the DOM the entire time, so waiting on elements to be available also does not work.
+    cy.wait(1000);  
  
     cy.get('@repoList').find(orgRepositoriesNameSelector).each((a) => {
       repoNames.push(a.text().trim());
@@ -40,23 +48,24 @@ describe('test github repos', () => {
     }).then( () => {
       expect(repoNames).to.deep.equal(sortedRepoNames.sort());
     });
-  })
+  });
 
   it('shows the correct clone link for the last sorted repo', () => {
-    cy.get(orgRepositoriesListSelector).find('ul').as('repoList');
-    cy.get('@repoList').find(orgRepositoriesNameSelector).last().as('lastRepo');
-   
-    cy.get('@lastRepo').then($lastRepo => {
+    cy.get(repoFilterDropdownSelector).contains('Sort').click();
+    cy.get(repoFilterItemSelector).contains('Name').click();
+    cy.get('@repoList').find(orgRepositoriesNameSelector).last().as('lastRepo')
+      .then($lastRepo => {
+        //Store the name of the last repo for link verification before clicking on it
       const lastRepoName = $lastRepo.text().trim();
       cy.wrap(lastRepoName).as('lastRepoName');
-    })
-    cy.get('@lastRepo').click();
+    });
 
+    cy.get('@lastRepo').click();
     cy.get('get-repo').click();
     cy.get('button').contains('HTTPS');
+
     cy.get('@lastRepoName').then(lastRepoName => {
       cy.get('div[role=tabpanel]').find('clipboard-copy').should('have.value', `https://github.com/indigov-us/${lastRepoName}.git`);
-    })
-  })
-})
-
+    });
+  });
+});
