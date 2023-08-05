8daef5328b707ace381b1b2a4e295b43f5923268
Katie Hull <katie.ann.hull@gmail.com>
2023-08-04 21:55:20 -0700

    Refactoring selectors into constants

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,14 @@
-var orgName = 'Indigov';
-var expectedTotalRepos = '17';
-var expectedTsRepos = '5';
+const repositorySelector = 'a[href="/orgs/indigov-us/repositories"]';
+const repositoryCountSelector = 'span[data-view-component=true]';
+
+const orgRepositoriesListSelector = 'div[id=org-repositories]';
+const repoFilterDropdownSelector = 'summary[role=button]';
+const repoFilterItemSelector = 'span[data-menu-button-text]';
+const orgRepositoriesNameSelector = 'a[data-hovercard-type=repository]';
+
+const orgName = 'Indigov';
+const expectedTotalRepos = '17';
+const expectedTsRepos = '5';
 var repoNames = [];
 var sortedRepoNames = [];
 
@@ -10,23 +18,23 @@ describe('test github repos', () => {
   })
 
   it('contains the expected number of repos', () => {
-    cy.get('a[href="/orgs/indigov-us/repositories"]').find('span[data-view-component=true]').should('contain.text', expectedTotalRepos);
+    cy.get(repositorySelector).find(repositoryCountSelector).should('contain.text', expectedTotalRepos);
   })
 
   it('contains the expected number of TypeScript repos', () => {
-    cy.get('summary[role=button]').contains('Language').click();
-    cy.get('span[data-menu-button-text]').contains('TypeScript').click();
-    cy.get('div[id=org-repositories]').find('div').contains('results').should('contain.text', expectedTsRepos);
+    cy.get(repoFilterDropdownSelector).contains('Language').click();
+    cy.get(repoFilterItemSelector).contains('TypeScript').click();
+    cy.get(orgRepositoriesListSelector).find('div').contains('results').should('contain.text', expectedTsRepos);
   })
   
   it('sorts the repos by name, descending', () => {
-    cy.get('summary[role=button]').contains('Sort').click();
-    cy.get('span[data-menu-button-text]').contains('Name').click();
-    cy.wait(1000);
+    cy.get(repoFilterDropdownSelector).contains('Sort').click();
+    cy.get(repoFilterItemSelector).contains('Name').click();
+    cy.wait(1000); //wait for list to re-render after sort
 
-    cy.get('div[id=org-repositories]').find('ul').as('repoList');
+    cy.get(orgRepositoriesListSelector).find('ul').as('repoList');
  
-    cy.get('@repoList').find('li').find('a[data-hovercard-type=repository]').each((a) => {
+    cy.get('@repoList').find(orgRepositoriesNameSelector).each((a) => {
       repoNames.push(a.text().trim());
       sortedRepoNames.push(a.text().trim());
     }).then( () => {
@@ -35,8 +43,8 @@ describe('test github repos', () => {
   })
 
   it('shows the correct clone link for the last sorted repo', () => {
-    cy.get('div[id=org-repositories]').find('ul').as('repoList');
-    cy.get('@repoList').find('a[data-hovercard-type=repository]').last().as('lastRepo');
+    cy.get(orgRepositoriesListSelector).find('ul').as('repoList');
+    cy.get('@repoList').find(orgRepositoriesNameSelector).last().as('lastRepo');
    
     cy.get('@lastRepo').then($lastRepo => {
       const lastRepoName = $lastRepo.text().trim();
